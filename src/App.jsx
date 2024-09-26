e63938ce5c8bc9561191560bd25d61ae585a765c
xMarioOP <marioalejandro424@outlook.com>
2024-09-26 17:25:40 -0500

    Improve dark mode persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Main from './components/Main'
 import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true)
+
+  const storageDarkMode = JSON.parse(localStorage.getItem("darkMode"))
+  console.log(localStorage);
+
+  const [darkMode, setDarkMode] = useState(storageDarkMode || false)
+
+  useEffect(() => localStorage.setItem("darkMode", JSON.stringify(darkMode)),[darkMode])
 
   function toggleDarkMode() {
     setDarkMode(prevMode => !prevMode)
-  }
+  } 
 
   return (
     <div className="container">
